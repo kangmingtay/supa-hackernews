9075e5537ce241ff05eb4f749ced7c842eee110d
Kang Ming <kang.ming1996@gmail.com>
2021-08-06 17:51:32 +0800

    add sql functions to fetch new & best stories

---
+++ sql/get_best_stories.sql
@@ -0,0 +1,29 @@
+-- drop function get_new_stories ()
+create function get_best_stories ()
+returns void as
+$$
+declare
+    sid bigint;
+	story json;
+begin
+    for sid in select json_array_elements(content::json) from http_get('https://hacker-news.firebaseio.com/v0/beststories.json')
+    loop 
+		select content::json into story from http_get('https://hacker-news.firebaseio.com/v0/item/' || sid || '.json');
+		insert into best_stories(by, id, url, kids, descendants, time, title, text, type, score, last_updated_at)
+		values (
+			story->>'by', 
+			cast(story->>'id' as bigint),
+			story->>'url', 
+			json_array_castint(story->'kids'), 
+			cast(story->>'descendants' as int),
+			to_timestamp(cast(story->>'time' as bigint)),
+			story->>'title',
+			story->>'text',
+			story->>'type',
+			cast(story->>'score' as int),
+            current_timestamp
+		)
+		on conflict (id) do update set last_updated_at = current_timestamp;
+    end loop;
+end;
+$$ language plpgsql
